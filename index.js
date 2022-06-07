540ab76cc9ffe7a22f1306072f2d7d92b1417294
Karolina Dudek <karolinadudek9@gmail.com>
2022-06-07 11:41:33 +0200

    tabs refactor

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
 const createTable = async (arguments, usersArray) => {
 
-  const table= document.getElementById('table');
-  let i = 0;
-  
-  for (const argument of arguments) {
-    
-    const tr = document.createElement('tr');
-    tr.innerHTML = `<td class="firstTd">
+    const table = document.getElementById('table');
+    let i = 0;
+
+    for (const argument of arguments) {
+
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td class="firstTd">
                         <ul>
                             <li>${argument.name}</li>
                         </ul>
@@ -16,36 +15,36 @@ const createTable = async (arguments, usersArray) => {
                         <ol class="users">
                         </ol>
                     </td>`
-    
-    table.appendChild(tr);
-    const filterUsersOfCompany = usersArray.filter( n => n.company ===  argument.uri);
-    
-    for (const filterElement of filterUsersOfCompany) {
-        const usersOfCompanyList = document.getElementsByClassName('users')[i];
-        const companyUser = document.createElement('li');
-        companyUser.innerHTML = `${filterElement.name}`
-        usersOfCompanyList.appendChild(companyUser);
+
+        table.appendChild(tr);
+        const filterUsersOfCompany = usersArray.filter(n => n.company === argument.uri);
+
+        for (const filterElement of filterUsersOfCompany) {
+            const usersOfCompanyList = document.getElementsByClassName('users')[i];
+            const companyUser = document.createElement('li');
+            companyUser.innerHTML = `${filterElement.name}`
+            usersOfCompanyList.appendChild(companyUser);
+        }
+        i++
     }
-  i++
-  }
 }
 
 (async () => {
 
-  const companiesResponse =  await fetch('http://localhost:3000/companies/');
-  const companiesList = await companiesResponse.json();
+    const companiesResponse = await fetch('http://localhost:3000/companies/');
+    const companiesList = await companiesResponse.json();
 
-  const usersResponse =  await fetch('http://localhost:3000/users/');
-  const usersList = await usersResponse.json();
-  const usersCompany = [];
+    const usersResponse = await fetch('http://localhost:3000/users/');
+    const usersList = await usersResponse.json();
+    const usersCompany = [];
 
-  for (const user of usersList) {
-    const {company} = user.uris;
-    const {name} = user;
-    usersCompany.push({name, company});
-  }
+    for (const user of usersList) {
+        const {company} = user.uris;
+        const {name} = user;
+        usersCompany.push({name, company});
+    }
 
-  await createTable(companiesList, usersCompany);
+    await createTable(companiesList, usersCompany);
 
 })();
 
